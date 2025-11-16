d88892f1fd6a5228a323e91dc138fa562f408c02
chris <chrisoquinn.2@gmail.com>
2025-11-16 13:26:12 -0800

    Scheduling utils added. Missing for some reason?

---
+++ src/gradient_quality_control/scheduling_utils.py
@@ -0,0 +1,27 @@
+from torch.optim.lr_scheduler import LambdaLR
+
+
+class NormWarmupScheduler(LambdaLR):
+    """Scheduler with inverted warmup for norm targets."""
+
+    def __init__(self, scheduler, num_warmup_steps,
+                 warmup_start=10.0, warmup_end=1.0):
+        self.wrapped_scheduler = scheduler
+        self.num_warmup_steps = num_warmup_steps
+        self.warmup_start = warmup_start
+        self.warmup_end = warmup_end
+
+        def warmup_lr(step):
+            progress = step / num_warmup_steps
+            return warmup_start + progress * (warmup_end - warmup_start)
+
+        super().__init__(scheduler.optimizer, warmup_lr, last_epoch=-1)
+
+    def step(self):
+        self.wrapped_scheduler.step()
+
+        if self.last_epoch < self.num_warmup_steps - 1:
+            super().step()
+
+    def __getattr__(self, name):
+        return getattr(self.wrapped_scheduler, name)
