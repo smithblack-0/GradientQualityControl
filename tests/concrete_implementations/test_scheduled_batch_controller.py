c9676bdad6b706b81005755f4f7e45c1ad8c1d9a
chris <chrisoquinn.2@gmail.com>
2026-01-17 16:04:54 -0800

    Minor ruff oversight.

--- tests/concrete_implementations/test_scheduled_batch_controller.py
+++ tests/concrete_implementations/test_scheduled_batch_controller.py
@@ -31,6 +31,7 @@ import torch.distributed as dist
 import torch.multiprocessing as mp
 import torch.nn as nn
 import torch_schedule_anything as tsa
+
 from src.gradient_quality_control.implementations.schedule_batch_controller import (
     OptimizerWrapperSBC,
     make_sbc_with_polynomial_schedule,
